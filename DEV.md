004836d74a43eaa6efd6f77fa1f165b86dcc110a
Tanner Lake <tanner.lake@gmail.com>
2020-04-23 17:42:59 -0700

    update deploy script

--- DEV.md
+++ DEV.md
@@ -14,7 +14,7 @@ Use `hugo server` to run a dev server on `localhost:1313`, and commit changes as
 
 ## Deploying
 
-Once changes have been made and committed, run the `deploy.sh` script from the `src/` directory.
+Once changes have been made and committed, run the `deploy.sh` script.
 This will configure the environment and build the site's HTML and XML with `hugo -v` in accordance with the `config.toml` settings.
 The built site resides in `docs/`, and once the site has been rebuilt, the script automatically commits those changes and then pushes to master.
 Then, GitHub will automatically serve those changes, and the live site will be updated in a minute or so.
--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SRC_DIR="${REPO_ROOT}/src"
+
 echo "Deploying updates to GitHub..."
 
-echo "Building site"
-cd ./src
+echo "- Building site"
+cd ${SRC_DIR}
 HUGO_ENV=production hugo -v
-cd ..
+cd ${REPO_ROOT}
 
-echo "Adding changes to git"
+echo "- Adding changes to git"
 git add ./docs
 
 MESSAGE="Rebuilding docs for `date` deployment"
@@ -15,10 +18,10 @@ if [ $# -eq 1 ] ; then
     MESSAGE="$1"
 fi
 
-echo "Committing changes to git"
+echo "- Committing changes to git"
 git commit -m "$MESSAGE"
 
-echo "Pushing changes to git"
+echo "- Pushing changes to git"
 git push origin master
 
 echo "Deployment complete"
